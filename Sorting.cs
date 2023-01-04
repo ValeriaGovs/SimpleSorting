ad94769055d4af05e375262e5052bab479e35788
lera <lera@rbi.ru>
2023-01-04 13:36:46 +0300

    тестирование

---
+++ Sorting.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleSorting
+{
+    public class Sorting
+    {
+
+        public  void ShellSort(int[] mass)
+        {
+
+
+            int i, j, step;
+            int tmp;
+            int n = mass.Length;
+            for (step = n / 2; step > 0; step /= 2)
+                for (i = step; i < n; i++)
+                {
+                    tmp = mass[i];
+                    for (j = i; j >= step; j -= step)
+                    {
+                        if (tmp < mass[j - step])
+                            mass[j] = mass[j - step];
+                        else
+                            break;
+                    }
+                    mass[j] = tmp;
+                }
+
+
+            printMass(mass);
+
+        }
+
+        public  void InsertionSort(int[] mass)
+        {
+
+
+
+            for (int i = 0; i < mass.Length - 1; i++)
+            {
+                int n_min = i + 1;
+
+                for (int j = i; j >= 0; j--)
+                {
+
+                    if (mass[j] <= mass[n_min])
+                    {
+                        break;
+                    }
+
+                    var k = mass[n_min];
+                    mass[n_min] = mass[j];
+                    mass[j] = k;
+                    n_min = j;
+                }
+            }
+
+            printMass(mass);
+        }
+
+        public  void InsertionSortImproved(int[] mass)
+        {
+
+
+            for (int i = 0; i < mass.Length - 1; i++)
+            {
+                int n_min = i + 1;
+                var inserted = mass[n_min];
+
+                for (int j = i; j >= 0; j--)
+                {
+
+                    if (mass[j] <= inserted)
+                    {
+                        break;
+                    }
+
+                    mass[j + 1] = mass[j];
+                    n_min = j;
+                }
+
+                if (mass[n_min] != inserted)
+                {
+                    mass[n_min] = inserted;
+                }
+            }
+
+
+            printMass(mass);
+        }
+
+
+
+        public  void BubbleSort(int[] mass)
+        {
+            for (int j = 0; j < mass.Length; j++)
+            {
+                for (int i = 0; i < mass.Length; i++)
+                {
+                    if (i == 0)
+                    { }
+                    else if (mass[i] < mass[i - 1])
+                    {
+                        var k = mass[i];
+                        mass[i] = mass[i - 1];
+                        mass[i - 1] = k;
+                    }
+                }
+            }
+
+
+
+            printMass(mass);
+        }
+
+
+        private static void printMass(int[] mass)
+        {
+            foreach (int i in mass)
+            {
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine(" ");
+        }
+
+    }
+}
---
+++ timeTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_LinearSorting
+{
+    public class timeTest
+    {
+        public void Test(func f, string funcName)
+        {
+            Console.WriteLine($" ");
+            Console.WriteLine($" ");
+
+
+            Stopwatch clock = new Stopwatch();
+
+            int count = 0;
+            int n_max = 10; //1_000_000;
+            for (int n = 10; n <= n_max; n *= 10)
+            {
+
+                //creating an array
+                Random r = new Random();
+                var unsort = new int[n];
+                for (int i = 0; i < n; i++)
+                {
+                    unsort[i] = r.Next(0, 999);
+                }
+
+                clock.Start();
+
+                // our action
+                f(unsort);
+
+
+                Console.Write($" до {n}  {funcName}: {count}");
+                clock.Stop();
+
+
+
+
+                TimeSpan ts = clock.Elapsed;
+                string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                    ts.Hours, ts.Minutes, ts.Seconds,
+                    ts.Milliseconds / 10);
+                Console.WriteLine($"{elapsedTime} ");
+            }
+        }
+        public delegate void func(int[] m);
+    }
+}
